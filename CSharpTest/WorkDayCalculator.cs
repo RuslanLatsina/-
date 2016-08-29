1af80a595fe26e88758b48f8863d2bbb6f22fac5
Ruslan Latsina <avatarandruslan@gmail.com>
2016-08-29 13:12:02 +0300

    Add project files.

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime EndDate = new DateTime();
+            var startDatePlusDayCount = startDate.AddDays(dayCount);
+            int countWeekEnd = 0;
+            //обчислення кількості вихідних
+            if (weekEnds != null)
+            {
+                for (int i = 0; i < weekEnds.Length; i++)
+                {
+                    if ((startDate > weekEnds[i].StartDate && startDate > weekEnds[i].EndDate) ||
+                        (startDatePlusDayCount < weekEnds[i].StartDate && startDatePlusDayCount < weekEnds[i].EndDate))
+                    {
+                        countWeekEnd += 0;
+                    }
+                    else if (startDate == weekEnds[i].EndDate || startDatePlusDayCount == weekEnds[i].StartDate)
+                    {
+                        countWeekEnd++;
+                    }
+                    else if (startDate <= weekEnds[i].StartDate && startDatePlusDayCount >= weekEnds[i].EndDate)
+                    {
+                        if (startDate != weekEnds[i].StartDate && weekEnds[i].Diff() != 1)
+                            countWeekEnd += weekEnds[i].Diff() + 1;
+                        else
+                            countWeekEnd += weekEnds[i].Diff();
+                        }
+                    else if ((startDate > weekEnds[i].StartDate &&
+                             (startDate < weekEnds[i].EndDate && startDatePlusDayCount >= weekEnds[i].EndDate))
+                             ||
+                             (startDatePlusDayCount < weekEnds[i].EndDate &&
+                             (startDate <= weekEnds[i].StartDate && startDatePlusDayCount > weekEnds[i].StartDate) ))
+                        countWeekEnd += (int) (weekEnds[i].EndDate - startDate).TotalDays ;
+                    else if(weekEnds[i].StartDate < startDate && startDatePlusDayCount < weekEnds[i].EndDate)
+                        countWeekEnd += (int)(weekEnds[i].EndDate - startDate).TotalDays;
+                    }
+            }
+            else
+                countWeekEnd += 0;
+
+            //обчислення кінцевої дати
+            dayCount += countWeekEnd - 1;
+            EndDate = startDate.AddDays(dayCount);
+
+            return EndDate;
+        }
+
+    }
+    //обчислення вихідного
+    public static class Extensions
+    {
+        public static int Diff(this WeekEnd w)
+        {
+            var days = (int)(w.EndDate - w.StartDate).TotalDays;
+            return days == 0 ? 1 : days;
+        }
+    }
+
+}
---
+++ CSharpTest/WorkDayCalculatorTests.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            // початкова дата
+            //перший день робочий
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+           
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+
+
+            // враховуємо початковий день, як робочий, тому 
+            //вираховуємо дату так startDate.AddDays(count - 1)
+            var expectedResult = startDate.AddDays(count - 1);
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void TestTwoWeekEnds()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 12, 2);
+            DateTime endWeekend = new DateTime(2015, 12, 4);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            DateTime startWeekend1 = new DateTime(2015, 12, 7);
+            DateTime endWeekend1 = new DateTime(2015, 12, 9);
+            WeekEnd wekEnd1 = new WeekEnd(startWeekend1, endWeekend1);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd, wekEnd1 };
+
+            var expectedResult = new DateTime(2015,12,16);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+            
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [TestMethod]
+        public void TestOneWeekEnd()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 12, 2);
+            DateTime endWeekend = new DateTime(2015, 12, 4);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd};
+
+            var expectedResult = new DateTime(2015, 12, 13);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestOneWeekEnd1()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 11, 20);
+            DateTime endWeekend = new DateTime(2015, 12, 15);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd };
+
+            var expectedResult = new DateTime(2015, 12, 24);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestOneWeekEnd2()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 12, 1);
+            DateTime endWeekend = new DateTime(2015, 12, 11);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd };
+
+            var expectedResult = new DateTime(2015, 12, 20);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+        [TestMethod]
+        public void TestOneWeekEnd3()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 12, 1);
+            DateTime endWeekend = new DateTime(2015, 12, 21);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd };
+
+            var expectedResult = new DateTime(2015, 12, 30);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+
+        [TestMethod]
+        public void TestOneWeekEndAfterJob()
+        {
+            //вхідні дані
+            DateTime startDate = new DateTime(2015, 12, 1);
+            int count = 10;
+            DateTime startWeekend = new DateTime(2015, 12, 22);
+            DateTime endWeekend = new DateTime(2015, 12, 22);
+            WeekEnd wekEnd = new WeekEnd(startWeekend, endWeekend);
+            WeekEnd[] weekEnds = new WeekEnd[] { wekEnd };
+
+            var expectedResult = new DateTime(2015, 12, 10);
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekEnds);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+
+    }
+
+
+
+}
